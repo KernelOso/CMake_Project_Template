c735c2b8e8d94d8731e451aa89cf57893931753a
Oscar D Prieto O <oscardpotsu@gmail.com>
2025-06-14 21:21:30 -0500

    ➕(presets): Add SDL2 static preset!

---
+++ presets/sdl2/main.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <SDL.h>
+
+int main() {
+
+    // 1. Initialize SDL modules
+    if ( SDL_Init(SDL_INIT_VIDEO) != 0 ){
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // 2. Create window
+    SDL_Window *window = SDL_CreateWindow(
+            "Window Title",                   //Window Title
+            SDL_WINDOWPOS_CENTERED,    // X Window position on screen
+            SDL_WINDOWPOS_CENTERED,    // Y Window position on screen
+            800 , 600 ,             // X,Y Window size
+            SDL_WINDOW_SHOWN           //Visible mode
+        );
+
+    // 3. Verify window
+    if (!window){
+        SDL_Quit();
+        return 1;
+    }
+
+    bool isExecuting = true;
+    SDL_Event event;
+
+    // 4. Set the main execution loop
+    while (isExecuting) {
+
+        // 5. Event Treat
+        while (SDL_PollEvent(&event)) {
+
+            if (event.type == SDL_QUIT) {
+                isExecuting = false;
+            }
+
+        }
+
+        // 6. FPS Control
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
---
+++ presets/sdl2/project.md
@@ -0,0 +1,15 @@
+# SDL2 
+
+CMakeLists projects template for a C/C++ projects ussing [SDL2](https://github.com/libsdl-org/SDL) graphic library
+
+# Init your project :
+
+C :
+```
+curl -s https://raw.githubusercontent.com/KernelOso/CMake_Project_Template/refs/heads/main/scripts/init.sh | bash -s -- sdl2 c
+```
+
+C++
+```
+curl -s https://raw.githubusercontent.com/KernelOso/CMake_Project_Template/refs/heads/main/scripts/init.sh | bash -s -- sdl2 cpp
+```
